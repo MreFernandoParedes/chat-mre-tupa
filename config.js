27e05027ad8d73d904355e0344cef373679ed418
MreFernandoParedes <fparedesa@rree.gob.pe>
2025-10-28 15:39:45 -0500

    Update Azure API key to use secrets management
    Replaced hardcoded Azure API key with a placeholder for secrets management.

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ export const AZURE_PROJECT_ENDPOINT = "https://chattupa-mre-resource.services.ai
 
 // ⚠️ cuando generemos la clave, NO la pegues aquí directamente
 // Vamos a cargarla desde entorno / secrets de GitHub más adelante
-export const AZURE_API_KEY = "ALeUymD2KDiDOn2OKinseq6WZCDAIsjNfBuiM2MLIijWFZnUd0q9JQQJ99BJACHYHv6XJ3w3AAAAACOGBDZZ";
+export const AZURE_API_KEY = "<AGREGAR_CLAVE_EN SECRETO>";
 export const AGENT_ID = "asst_FwArxYY1DxTJ33uFmcvW5fmi";
 
